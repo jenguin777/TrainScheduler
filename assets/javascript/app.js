7bab6b3abf78382b1e82cd3a525effe11fb8e514
Jennifer Fahey <jenguin777@gmail.com>
2018-07-23 18:46:44 -0400

    clean up comments and formatting of code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,16 +10,23 @@ var config = {
 // This is passing in our firebase config information  
 firebase.initializeApp(config);
 
-// trainScheduler has a list of trains, each with a list of train attributes, i.e., array...this is one train
-var train = "";
-
 // VARIABLES
 // --------------------------------------------------------------------------------
-// put your firebase database into a variable called trainData
+// ***Firebase variables***
+
+// put firebase database into a variable called db
 var db = firebase.database();
+
+// create TrainScheduler folder in firebase database, then store in a variable called trainData
 var trainData = db.ref("/TrainScheduler");
 
-// Initial values for our actual variables
+// ***Program variables***
+
+// trainScheduler contains a list of trains, each train having a list of attributes
+// train is the variable for one train
+var train = "";
+
+// now for the train attribute variables
 // Name of train
 var trainName = "";
 // Destination
@@ -29,8 +36,51 @@ var firstTrainTime = "";
 // frequency train runs, in minutes
 var frequency = 0;
 
+// FUNCTIONS
 // --------------------------------------------------------------------------------
 
+// Fetch the train data from the firebase database and write it to the #schedule-section
+function appendTrain(train) {
+
+  console.log("train " + JSON.stringify(train));
+
+  var tr = $("<tr>");
+
+  var tdName = $("<td>");
+  tdName.text(train.val().nameData);
+  
+  var tdDestination = $("<td>");
+  tdDestination.text(train.val().destinationData);
+
+  var tdFrequency = $("<td>");
+  tdFrequency.text(train.val().frequencyData);
+  
+  tr.append(tdName).append(tdDestination).append(tdFrequency);
+
+  $("#schedule-section").append(tr);
+  
+}
+
+// BODY
+// --------------------------------------------------------------------------------
+
+// Firebase watcher + initial loader for the subsequent submits
+trainData.on("child_added", function(childsnapshot) {
+  console.log(childsnapshot.val());
+
+  // Log everything that's coming out of childsnapshot
+  console.log(childsnapshot.val().nameData);
+  console.log(childsnapshot.val().destinationData);
+  console.log(childsnapshot.val().firstTrainTimeData);
+  console.log(childsnapshot.val().frequencyData);
+
+  // Call appendTrain() to write results to the #schedule-section
+  appendTrain(childsnapshot);
+
+}, function(errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+  
 // Handling the add-train Submit button clicks
 $("#add-train").on("click", function(event) {
   event.preventDefault();
@@ -51,45 +101,4 @@ $("#add-train").on("click", function(event) {
       });
   });
 
-  // Fetch the train data from the firebase database and write it to the #schedule-section
-  function appendTrain(train) {
-
-    console.log("train " + JSON.stringify(train));
-
-    var tr = $("<tr>");
-
-    var tdName = $("<td>");
-    tdName.text(train.val().nameData);
-    
-    var tdDestination = $("<td>");
-    tdDestination.text(train.val().destinationData);
-
-    var tdFrequency = $("<td>");
-    tdFrequency.text(train.val().frequencyData);
-    
-    tr.append(tdName).append(tdDestination).append(tdFrequency);
-
-    $("#schedule-section").append(tr);
-    
-  }
-
-// Firebase watcher + initial loader for the subsequent submits
-trainData.on("child_added", function(childsnapshot) {
-  console.log(childsnapshot.val());
-
-  // Log everything that's coming out of childsnapshot
-  console.log(childsnapshot.val().nameData);
-  console.log(childsnapshot.val().destinationData);
-  console.log(childsnapshot.val().firstTrainTimeData);
-  console.log(childsnapshot.val().frequencyData);
-
-  // Call appendTrain() to write results to the #schedule-section
-  appendTrain(childsnapshot);
-
-}, function(errorObject) {
-      console.log("The read failed: " + errorObject.code);
-    });
-
-
-  
   // --------------------------------------------------------------

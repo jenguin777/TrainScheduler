98602da0550dc41edcaed7635f81648d9c41851b
Jennifer Fahey <jenguin777@gmail.com>
2018-07-23 18:33:22 -0400

    removed for loop and fixed html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,7 @@ var config = {
 // This is passing in our firebase config information  
 firebase.initializeApp(config);
 
-// Adding this because train is stored as list of lists, i.e., array
+// trainScheduler has a list of trains, each with a list of train attributes, i.e., array...this is one train
 var train = "";
 
 // VARIABLES
@@ -29,7 +29,9 @@ var firstTrainTime = "";
 // frequency train runs, in minutes
 var frequency = 0;
 
-// Handling the add train Submit button clicks
+// --------------------------------------------------------------------------------
+
+// Handling the add-train Submit button clicks
 $("#add-train").on("click", function(event) {
   event.preventDefault();
       
@@ -51,84 +53,43 @@ $("#add-train").on("click", function(event) {
 
   // Fetch the train data from the firebase database and write it to the #schedule-section
   function appendTrain(train) {
-    // for (var i = 0; i < trainData.length; i++) {
-      //Object.keys(trainData).length
-    for (var i = 0; i < Object.keys(train).length; i++) {
+
+    console.log("train " + JSON.stringify(train));
+
     var tr = $("<tr>");
 
     var tdName = $("<td>");
-    tdName.text(train.nameData);
-    console.log("tdName " + JSON.stringify(tdName));
-
+    tdName.text(train.val().nameData);
+    
     var tdDestination = $("<td>");
-    tdDestination.text(train.destinationData);
-    console.log("tdDestination " + JSON.stringify(tdDestination));
+    tdDestination.text(train.val().destinationData);
 
     var tdFrequency = $("<td>");
-    tdFrequency.text(train.frequencyData);
-    console.log("tdFrequency " + JSON.stringify(tdFrequency));
+    tdFrequency.text(train.val().frequencyData);
     
     tr.append(tdName).append(tdDestination).append(tdFrequency);
 
     $("#schedule-section").append(tr);
-    }
-
+    
   }
-
-console.log("tdName " + JSON.stringify(train));
-appendTrain();
 
 // Firebase watcher + initial loader for the subsequent submits
 trainData.on("child_added", function(childsnapshot) {
   console.log(childsnapshot.val());
 
-  // Log everything that's coming out of snapshot
+  // Log everything that's coming out of childsnapshot
   console.log(childsnapshot.val().nameData);
   console.log(childsnapshot.val().destinationData);
   console.log(childsnapshot.val().firstTrainTimeData);
   console.log(childsnapshot.val().frequencyData);
 
   // Call appendTrain() to write results to the #schedule-section
-  appendTrain();
+  appendTrain(childsnapshot);
 
 }, function(errorObject) {
       console.log("The read failed: " + errorObject.code);
     });
 
-// // --------------------------------------------------------------
-// // MAIN PROCESS and INITIAL CODE AT STARTUP
-// // At the initial load and on subsequent data value changes, get a snapshot of the current data. (I.E FIREBASE HERE)
-// // This callback keeps the page updated when a value changes in firebase.
-// trainData.on("value", function(snapshot) {
-//     // We are now inside our .on function...
-  
-//     // Console.log the "snapshot" value (a point-in-time representation of the database)
-//     console.log(snapshot.val());
-//     // This "snapshot" allows the page to get the most current values in firebase.
-  
-//     // Change the value of our variables to match the values in the database
-//     trainName = snapshot.val().nameData;
-//     destination = snapshot.val().destinationData;
-//     firstTrainTime = snapshot.val().firstTrainTimeData;
-//     frequency = snapshot.val().frequencyData;
-  
-//     // Console Log the value of the train my variables
-//     console.log(trainName);
-//     console.log(destinationData);
-//     console.log(firstTrainTime);
-//     console.log(frequency);
-  
-//     // Change the HTML using jQuery to reflect the updated values - need to add all of the info for #schedule-section here...attr? append???
-//     $("#schedule-section").text(snapshot.val().nameData);
 
-//     //Maybe need to use attr() or append()?
-//     $("#schedule-section").text(snapshot.val().destinationData);
-//     $("#schedule-section").text(snapshot.val().firstTrainTimeData);
-//     $("#schedule-section").text(snapshot.val().frequencyData);
-  
-//   // If any errors are experienced, log them to console.
-//   }, function(errorObject) {
-//     console.log("The read failed: " + errorObject.code);
-//   });
   
   // --------------------------------------------------------------
